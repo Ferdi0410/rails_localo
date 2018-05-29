4a7ad2b0c591488085b51bf64e078e670fcdf644
Tony <tony.xing.91@gmail.com>
2018-05-29 12:02:50 +0200

    routing with filter done

--- app/models/tour.rb
+++ app/models/tour.rb
@@ -1,2 +1,4 @@
 class Tour < ApplicationRecord
+  has_many :users, through: :user_tours
+  has_many :user_tours, dependent: :destroy
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Tour.destroy_all
+User.destroy_all
+
+u1 = User.new(
+  {email: "lito@gmail.com", password:"1111111", first_name: "Lito", username: "Litotechie"}
+  )
+
+u1.save
+
+t1 = Tour.new(
+  {title:"tour 1", duration: 60, description:"this is a test tour", category:"art", city:"Berlin"}
+  )
+t1.save
+
+t1 = Tour.new(
+  {title:"tour 2", duration: 60, description:"this is a test tour", category:"urban", city:"Berlin"}
+  )
+t1.save
