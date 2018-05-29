bcda0bd0b70cc2cf3eae191dc4c2cd7d6ff8fb8c
Emanuel Coen <emanuelcoen@gmail.com>
2018-05-29 17:11:13 +0200

    Created card carousel for the home page

---
+++ app/javascript/components/carousel.js
@@ -0,0 +1,13 @@
+function animateCarousel() {
+  // const carouselEl = document.getElementById('homePageCarousel');
+  // if (carouselEl) {
+  //   carouselEl.carousel();
+  // }
+  $('#homePageCarousel').carousel({
+    interval: 5000
+  })
+
+}
+export { animateCarousel };
+
+
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,1 +1,7 @@
+// External
 import "bootstrap";
+// Local
+import { animateCarousel } from "../components/carousel";
+
+// Animate my carousel
+animateCarousel();
