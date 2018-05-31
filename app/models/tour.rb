fce0164cf2056259ac7a7af5e61adae9dafb839e
Emanuel Coen <emanuelcoen@gmail.com>
2018-05-31 12:11:17 +0200

    cloudinary up and running

--- app/models/tour.rb
+++ app/models/tour.rb
@@ -3,4 +3,5 @@ class Tour < ApplicationRecord
   has_many :users, through: :user_tours
   has_many :tour_attractions
   has_many :attractions, through: :tour_attractions
+  mount_uploader :photo, PhotoUploader
 end
