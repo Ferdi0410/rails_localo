d7cd45bdcbc18cd7b956363131edba9f067df280
Ferdinand Rieger <Ferdinand.rieger@gmx.de>
2018-06-04 18:27:18 +0200

    map

--- app/javascript/packs/map.js
+++ app/javascript/packs/map.js
@@ -1,41 +1,61 @@
 import GMaps from 'gmaps/gmaps.js';
 
 
-let userPosition = { lat: 0, lng: 0, icon: new google.maps.MarkerImage('//maps.gstatic.com/mapfiles/mobile/mobileimgs2.png',
+
+
+function buildMap() {
+  const mapElement = document.getElementById('map');
+  if (mapElement) { // don't try to build a map if there's no div#map to inject in
+    const map = new GMaps({ el: '#map', lat: 0, lng: 0 });
+    trackMe(map)
+    const markers = JSON.parse(mapElement.dataset.markers);
+    map.addMarkers(markers);
+    if (markers.length === 0) {
+      map.setZoom(2);
+    } else if (markers.length === 1) {
+      // map.setCenter(markers[0].lat, markers[0].lng);
+      map.setZoom(10);
+    } else {
+      map.fitLatLngBounds(markers);
+    }
+    // setTimeout(buildMap, 10000);
+  }
+}
+buildMap();
+
+// function setUserPosition(map) {
+//   GMaps.geolocate({
+//     success: function(position) {
+//       userPosition.lat = position.coords.latitude
+//       userPosition.lng = position.coords.longitude
+//       map.setCenter(position.coords.latitude, position.coords.longitude);
+//       map.addMarker(userPosition)
+//       trackMe()
+//     }
+//   })
+// }
+
+function trackMe(map) {
+  let userPosition = { lat: 0, lng: 0, icon: new google.maps.MarkerImage('//maps.gstatic.com/mapfiles/mobile/mobileimgs2.png',
                                                     new google.maps.Size(22,22),
                                                     new google.maps.Point(0,18),
                                                     new google.maps.Point(11,11)),
                                                      }
-
-function autoUpdate() { const mapElement = document.getElementById('map');
-if (mapElement) { // don't try to build a map if there's no div#map to inject in
-  const map = new GMaps({ el: '#map', lat: 0, lng: 0 });
-  setUserPosition(map)
-  const markers = JSON.parse(mapElement.dataset.markers);
-  map.addMarkers(markers);
-  if (markers.length === 0) {
-    map.setZoom(2);
-  } else if (markers.length === 1) {
-    map.setCenter(markers[0].lat, markers[0].lng);
-    map.setZoom(10);
-  } else {
-    map.fitLatLngBounds(markers);
-  }
-  setTimeout(autoUpdate, 10000000);
-}
-}
-autoUpdate();
-
-function setUserPosition(map) {
-  GMaps.geolocate({
-    success: function(position) {
-      userPosition.lat = position.coords.latitude
-      userPosition.lng = position.coords.longitude
+  if (navigator.geolocation) {
+    // this.isTracking = true;
+      const userMarker = map.addMarker(userPosition)
+    navigator.geolocation.watchPosition((position) => {
+      const newPosition = new google.maps.LatLng({lat: position.coords.latitude, lng: position.coords.longitude});
+      console.log('new position')
+      console.log(position.coords)
       map.setCenter(position.coords.latitude, position.coords.longitude);
-      map.addMarker(userPosition)
-    }
-  })
+      userMarker.setPosition(newPosition)
+    });
+  } else {
+    alert("Geolocation is not supported by this browser.");
+  }
 }
+
 
 // map.drawRoute({
 //   origin: [-12.044012922866312, -77.02470665341184],
