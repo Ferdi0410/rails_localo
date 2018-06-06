f0457afee9b683fc0cbcf69db55432b570a54172
Ferdinand Rieger <Ferdinand.rieger@gmx.de>
2018-06-06 14:48:01 +0200

    Fixed Lito's Mistake ..... :D

--- app/javascript/packs/map.js
+++ app/javascript/packs/map.js
@@ -1,16 +1,46 @@
-import GMaps from 'gmaps/gmaps.js';
+ import GMaps from 'gmaps/gmaps.js';
 
-const mapElement = document.getElementById('map');
-if (mapElement) { // don't try to build a map if there's no div#map to inject in
-  const map = new GMaps({ el: '#map', lat: 0, lng: 0 });
-  const markers = JSON.parse(mapElement.dataset.markers);
-  map.addMarkers(markers);
-  if (markers.length === 0) {
-    map.setZoom(2);
-  } else if (markers.length === 1) {
-    map.setCenter(markers[0].lat, markers[0].lng);
-    map.setZoom(14);
-  } else {
-    map.fitLatLngBounds(markers);
-  }
+
+
+
+function buildMap() {
+  const mapElement = document.getElementById('map');
+  if (mapElement) { // don't try to build a map if there's no div#map to inject in
+   const map = new GMaps({ el: '#map', lat: 0, lng: 0 });
+   trackMe(map)
+   const markers = JSON.parse(mapElement.dataset.markers);
+   map.addMarkers(markers);
+   if (markers.length === 0) {
+     map.setZoom(2);
+   } else if (markers.length === 1) {
+     // map.setCenter(markers[0].lat, markers[0].lng);
+     map.setZoom(10);
+   } else {
+     map.fitLatLngBounds(markers);
+   }
+   // setTimeout(buildMap, 10000);
+ }
 }
+buildMap();
+
+
+function trackMe(map) {
+ let userPosition = { lat: 0, lng: 0, icon: new google.maps.MarkerImage('//maps.gstatic.com/mapfiles/mobile/mobileimgs2.png',
+                                                   new google.maps.Size(22,22),
+                                                   new google.maps.Point(0,18),
+                                                   new google.maps.Point(11,11)),
+                                                    }
+ if (navigator.geolocation) {
+   // this.isTracking = true;
+     const userMarker = map.addMarker(userPosition)
+   navigator.geolocation.watchPosition((position) => {
+     updateCurrentPosition(map, userMarker, position)
+   });
+
+function updateCurrentPosition(map, marker, position) {
+const newPosition = new google.maps.LatLng({lat: position.coords.latitude, lng: position.coords.longitude});
+map.setCenter(position.coords.latitude, position.coords.longitude);
+marker.setPosition(newPosition)
+}
+  }
+ }
